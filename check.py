fe9f221aa0c14818a5784de1680d7241c2ec16fd
Kiran Anchule <32509476+kirananchule@users.noreply.github.com>
2025-05-15 19:31:53 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,1 @@
-from google import genai
 
-client = genai.Client(api_key="AIzaSyA7JYtN2KUGpOzlNfFoS1tle9VRETJ9S1Y")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
